48ce6ba0e102cbf0e1627d204b3f7340e73ee441
Abhijit <tradeabhiyt@gmail.com>
2026-08-01 08:22:44 +0530

    Initial commit

---
+++ yourtube/src/pages/_app.tsx
@@ -0,0 +1,25 @@
+import Header from "@/components/Header";
+import Sidebar from "@/components/Sidebar";
+import { Toaster } from "@/components/ui/sonner";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { useState } from "react";
+import { UserProvider } from "../lib/AuthContext";
+export default function App({ Component, pageProps }: AppProps) {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  return (
+    <UserProvider>
+      <div className="min-h-screen bg-white text-black overflow-x-hidden">
+        <title>Your-Tube Clone</title>
+        <Header onMenuClick={() => setSidebarOpen((v) => !v)} />
+        <Toaster />
+        <div className="flex">
+          <Sidebar open={sidebarOpen} onClose={() => setSidebarOpen(false)} />
+          <div className="flex-1 min-w-0">
+            <Component {...pageProps} />
+          </div>
+        </div>
+      </div>
+    </UserProvider>
+  );
+}
